2ef18ace5d1a6df8b3f081d1a16717e4a0832b73
Roman Sinitsyn <romasini@gmail.com>
2020-09-21 09:44:56 +0300

    Initial commit

---
+++ src/main/java/ru/romasini/easy/shop/EasyShopApplication.java
@@ -0,0 +1,13 @@
+package ru.romasini.easy.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/romasini/easy/shop/EasyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.romasini.easy.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
