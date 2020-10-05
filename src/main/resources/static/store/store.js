1d524f4ccde05de596c1aaff0ce5727fd35d8380
Roman Sinitsyn <romasini@gmail.com>
2020-10-05 21:45:32 +0300

    2) попытка сделать пагинацию смог сделать кнопки Вперел/Назад и работают они только при включенном фильтре

--- src/main/resources/static/store/store.js
+++ src/main/resources/static/store/store.js
@@ -1,7 +1,26 @@
 angular.module('app').controller('storeController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189';
 
+    $scope.backPage = function(){
+        $scope.page = $scope.Products.number;
+        $scope.applyFilter();
+    }
+
+    $scope.forwardPage= function(){
+         $scope.page = $scope.Products.number+2;
+         if($scope.page>$scope.Products.totalPages){
+            $scope.page=$scope.Products.totalPages;
+         }
+         $scope.applyFilter();
+    }
+
+     $scope.inPage = function(newPage){
+             $scope.page = newPage;
+             $scope.applyFilter();
+        }
+
     $scope.fillTable = function () {
+        $scope.page = 1;
         $scope.filter=null;
         console.log('fill');
         $http.get(contextPath + '/api/v1/products')
@@ -14,7 +33,7 @@ angular.module('app').controller('storeController', function ($scope, $http) {
          $http({
              url: contextPath + '/api/v1/products',
              method: "GET",
-             params: {  p: 0,//$scope.filter.page,
+             params: {  p: $scope.page,
                         minPrice: $scope.filter.min_price,
                         maxPrice: $scope.filter.max_price,
                         titlePart: $scope.filter.title_part
