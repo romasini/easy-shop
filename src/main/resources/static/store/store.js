3217285310f14c197dea994f6c9e2b3a5e85941d
Roman Sinitsyn <romasini@gmail.com>
2020-10-08 09:07:36 +0300

    доработана пагинация, рефактор Фильтра

--- src/main/resources/static/store/store.js
+++ src/main/resources/static/store/store.js
@@ -1,47 +1,30 @@
 angular.module('app').controller('storeController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189';
 
-    $scope.backPage = function(){
-        $scope.page = $scope.Products.number;
-        $scope.applyFilter();
-    }
+      $scope.fillTable = function (pageIndex = 1) {
+            $http({
+                url: contextPath + '/api/v1/products',
+                method: 'GET',
+                params: {
+                    titlePart: $scope.filter ? $scope.filter.title : null,
+                    minPrice: $scope.filter ? $scope.filter.min_price : null,
+                    maxPrice: $scope.filter ? $scope.filter.max_price : null,
+                    p: pageIndex
+                }
+            })
+                .then(function (response) {
+                    $scope.ProductsPage = response.data;
+                    $scope.PaginationArray = $scope.generatePagesInd(1, $scope.ProductsPage.totalPages);
+                });
+      };
 
-    $scope.forwardPage= function(){
-         $scope.page = $scope.Products.number+2;
-         if($scope.page>$scope.Products.totalPages){
-            $scope.page=$scope.Products.totalPages;
-         }
-         $scope.applyFilter();
-    }
-
-     $scope.inPage = function(newPage){
-             $scope.page = newPage;
-             $scope.applyFilter();
-        }
-
-    $scope.fillTable = function () {
-        $scope.page = 1;
-        $scope.filter=null;
-        console.log('fill');
-        $http.get(contextPath + '/api/v1/products')
-               .then(function (response) {
-                $scope.Products = response.data;
-            });
-    };
-
-     $scope.applyFilter = function () {
-         $http({
-             url: contextPath + '/api/v1/products',
-             method: "GET",
-             params: {  p: $scope.page,
-                        minPrice: $scope.filter.min_price,
-                        maxPrice: $scope.filter.max_price,
-                        titlePart: $scope.filter.title_part
-                        }
-         }).then(function (response) {
-              $scope.Products = response.data;
-         });
-     }
+      $scope.generatePagesInd = function(startPage, endPage) {
+            let arr = [];
+            for (let i = startPage; i < endPage + 1; i++) {
+                arr.push(i);
+            }
+            return arr;
+      }
 
     $scope.fillTable();
 });
